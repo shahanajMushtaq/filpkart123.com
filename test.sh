57b53ada213d0265d9722ce71846834d52d18c2a
Mushtaq <mailmuahmed1980@gmail.com>
2020-09-08 22:39:54 +0000

    new change

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 v_pass=`cat password`
 echo "connecting to db"
-mysql -h dbhost.filpkart.com -u dbuser -p $v_pass dev-filpkart <<EOF
+mysql -h dbhost1.filpkart.com -u dbuser -p $v_pass dev-filpkart <<EOF
 select * from hr.employee;
 EOF
 
